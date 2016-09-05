cc836e3b5a598d883e33081f2ec52d62bb905e02
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-09-05 19:27:16 +0200

    add examples for html files

--- example/example.js
+++ example/example.js
@@ -1,7 +1,8 @@
 const pdf = require('../')
 const fs = require('fs')
+const path = require('path')
 
-pdf.create('<h1>hyperpdf</h1>').toFile('example/hyper.pdf', () => {
+pdf.create('<h1>hyperpdf</h1>').toFile('example/hyper_1.pdf', () => {
 
 })
 
@@ -10,5 +11,20 @@ pdf.create('<h1>hyperpdf</h1>').toBuffer((buf) => {
 })
 
 pdf.create('<h1>hyperpdf</h1>').toStream((stream) => {
-  stream.pipe(fs.createWriteStream('./foo.pdf'))
+  stream.pipe(fs.createWriteStream('./example/foo_1.pdf'))
+})
+
+
+const pathToFile = path.resolve(path.parse(__filename).dir, './card.html')
+
+pdf.create(pathToFile).toFile('example/hyper_2.pdf', () => {
+
+})
+
+pdf.create(pathToFile).toBuffer((buf) => {
+  console.log(buf.length)
+})
+
+pdf.create(pathToFile).toStream((stream) => {
+  stream.pipe(fs.createWriteStream('./example/foo_2.pdf'))
 })
