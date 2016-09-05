7c1ca2a84dc35b04a6294b0658fd5e242da9cddd
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-09-05 18:34:08 +0200

    add working examples

---
+++ example/example.js
@@ -0,0 +1,10 @@
+const pdf = require('../')
+const fs = require('fs')
+
+pdf.create('<h1>hyperpdf</h1>').toFile('example/hyper.pdf', () => {
+
+})
+
+pdf.create('<h1>hyperpdf</h1>').toBuffer((buf) => {
+  console.log(buf.length)
+})
