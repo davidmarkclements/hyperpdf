c5899c327a318c41a2167b224b3cb2553b20e6c0
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-09-12 10:00:10 +0200

    add stream test

---
+++ test/e2e/toStream.spec.js
@@ -0,0 +1,26 @@
+const pdf = require('../../')
+const fs = require('fs')
+const test = require('tape')
+
+
+test('stream pdf into file', (t) => {
+  pdf.create('<h1>hyperpdf</h1>').toStream((stream) => {
+    stream
+      .pipe(fs.createWriteStream('test/simpleHeading_stream.pdf'))
+      .on('err', (err) => { throw err })
+      .on('close', () => {
+        fs.readFile('test/simpleHeading_stream.pdf', (err, result) => {
+          if (err) {
+            throw err
+          }
+          fs.readFile('test/fixtures/simpleHeading.pdf', (err, buf) => {
+            if (err) {
+              throw err
+            }
+            t.equal(result.length, buf.length)
+            t.end()
+          })
+        })
+      })
+  })
+})
